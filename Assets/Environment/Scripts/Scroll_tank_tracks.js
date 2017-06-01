175445462dcb7315576ad38ea4857cdecc9dbc18
达达Mr_X <Mr_X>
2017-06-01 15:13:05 +0800

    a

---
+++ Assets/Environment/Scripts/Scroll_tank_tracks.js
@@ -0,0 +1,9 @@
+﻿var scrollSpeed :float = 10; 
+
+function Update () { 
+var offset : float = Time.time * scrollSpeed * Time.deltaTime % 1;
+
+GetComponent.<Renderer>().material.SetTextureOffset ("_MainTex", Vector2(0, offset)); 
+
+
+}
